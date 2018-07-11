41534ef215a77aef0e6d2e6a073a69bb6f319999
David Borah <dborah87@gmail.com>
2018-07-10 21:01:02 -0400

    Added some basic CSS

--- src/AirbnbCard.js
+++ src/AirbnbCard.js
@@ -23,18 +23,18 @@ class AirbnbCard extends Component {
           <img src="https://via.placeholder.com/250x250" />
         </section>
         <section className="AirbnbCard-location">
-        <h3>Location</h3>
+        <h3>Location: Rome, Italy, Earth, Milkyway!</h3>
         </section>
           <section className="AirbnbCard-mainheading">
-          <h1>Main heading</h1>
+          <h1>Visit the Pope and eat some pizza!</h1>
         </section>
         <section className="AirbnbCard-pricing">
-        <h4>Pricing</h4>
+        <h4>Pricing: $333.00</h4>
         </section>
         <section className="AirbnbCard-ratings">
           <button className="AirbnbCard-button addStars"
           onClick={this.addStars.bind(this)}>
-Ratings
+Total Ratings
           </button>
           <span>&#9734;&#9734;&#9734;&#9734;&#9734;</span>
           <span className="addStars circle">
