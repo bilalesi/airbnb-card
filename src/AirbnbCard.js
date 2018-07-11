8122f8f84982b28c539ee14ed52db5f962272b3f
David Borah <dborah87@gmail.com>
2018-07-10 20:56:06 -0400

    stubbed out a workign version that counts the 5 star ratings each time the ratings button is clicked

--- src/AirbnbCard.js
+++ src/AirbnbCard.js
@@ -6,15 +6,41 @@ class AirbnbCard extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        likes: 0,
-        dislikes: 0,
+        ratings: 0,
       };
     }
+
+  addStars(){
+      this.setState({
+          ratings: this.state.ratings  + 1
+      })
+  }
 
     render() {
       return (
         <div className="AirbnbCard-container">
-          <span>Hello</span>
+        <section className="AirbnbCard-image">
+          <img src="https://via.placeholder.com/250x250" />
+        </section>
+        <section className="AirbnbCard-location">
+        <h3>Location</h3>
+        </section>
+          <section className="AirbnbCard-mainheading">
+          <h1>Main heading</h1>
+        </section>
+        <section className="AirbnbCard-pricing">
+        <h4>Pricing</h4>
+        </section>
+        <section className="AirbnbCard-ratings">
+          <button className="AirbnbCard-button addStars"
+          onClick={this.addStars.bind(this)}>
+Ratings
+          </button>
+          <span>&#9734;&#9734;&#9734;&#9734;&#9734;</span>
+          <span className="addStars circle">
+            {this.state.ratings}
+          </span>
+        </section>
         </div>
       );
     }
